9aa040717587c5ec7d4537fe97aaae7f54232aff
Nicolas Orgias <norgias@gmail.com>
2025-01-04 15:39:54 -0500

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "norgias@gmail.com";
+    $subject = "New Message from Website Contact Form";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you for your message. We'll get back to you soon!";
+    } else {
+        echo "Sorry, something went wrong. Please try again.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
